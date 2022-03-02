84cda22c8446627becad3c8fcf85e41bdb6998fc
cleandever <cleandever@gmail.com>
2022-03-03 07:30:08 +0900

    287. Find the Duplicate Number, add floyd cycle detection solution

--- leetcode/01_medium/0287. Find the Duplicate Number.py
+++ leetcode/01_medium/0287. Find the Duplicate Number.py
@@ -20,10 +20,32 @@ class Solution:
                 return abs(nums[i])
         return -1
 
+    # floyd's cycle detection algorithm
+    def floyd_cycle_solution(self, nums):
+        slow = 0
+        fast = 0
+        while True:
+            # one step
+            slow = nums[slow]
+            # two step
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            # move one step both
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                break
+        return slow
+
+
+
     def findDuplicate(self, nums):
         # return self.sorting_solution(nums)
-        return self.index_solution(nums)
-
+        # return self.index_solution(nums)
+        return self.floyd_cycle_solution(nums)
 
 sol = Solution()
 assert sol.findDuplicate([1, 3, 4, 2, 2]) == 2
