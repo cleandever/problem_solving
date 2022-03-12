0b98d7f6feddf266e2355b33556cc936551eda5e
cleandever <cleandever@gmail.com>
2022-03-12 22:37:12 +0900

    콜라츠 추측

---
+++ programmers/level1/콜라츠추측.py
@@ -0,0 +1,21 @@
+"""
+https://programmers.co.kr/learn/courses/30/lessons/12943
+"""
+
+
+def solution(num):
+    count = 0
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num *= 3
+            num += 1
+        count += 1
+        if count >= 500:
+            break
+
+    return count if count < 500 else -1
+
+
+assert solution(6) == 8
